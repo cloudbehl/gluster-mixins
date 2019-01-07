273805ebc24f56467e30b1639d181534b9c66038
Ankush Behl <cloudbehl@gmail.com>
2019-01-08 01:29:28 +0530

    Added dockerfile to build the glustermixins container
    Signed-off-by: Ankush Behl <cloudbehl@gmail.com>

---
+++ docker/entry.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+sa_dir=/var/run/secrets/kubernetes.io/serviceaccount
+
+kc_args="--server=https://kubernetes.default.svc.cluster.local --token=$(cat $sa_dir/token) --certificate-authority=$sa_dir/ca.crt"
+
+kubectl $kc_args create -f manifests/
+
+if [ $? -ne 0 ]; then
+    echo "Failed to apply manifest/ folder"
+fi
+
+
